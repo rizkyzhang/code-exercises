a0c7ff592c10c3671983a3899ed72c29d5163caf
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2021-09-06 16:50:34 +0700

    feat: add project-euler problem-1

---
+++ project-euler/problem-1.js
@@ -0,0 +1,38 @@
+/*
+ * Title: Multiplies of 3 or 5
+ *
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+ * The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+ *
+ */
+
+/*
+ * Pseudocode
+ *
+ * SET sum to 0
+ *
+ * FOR each number starting from 3 until 999
+ *   IF number is multiple of 3 or 5
+ *      ADD number into sum
+ *   ENDIF
+ * ENDFOR
+ *
+ * RETURN sum
+ */
+
+function sumMul35() {
+  let sum = 0;
+
+  for (let i = 3; i < 1000; i++) {
+    const isMultipleOf3 = i % 3 === 0;
+    const isMultipleOf5 = i % 5 === 0;
+
+    if (isMultipleOf3 || isMultipleOf5) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+
+console.log(sumMul35());
