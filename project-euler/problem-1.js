cfd54c3ba331d79aac0c8c25c803dc9d8eb9c6cd
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2021-09-08 20:04:30 +0700

    feat: update project-euler problem-1

--- project-euler/problem-1.js
+++ project-euler/problem-1.js
@@ -1,8 +1,6 @@
 /*
  * Title: Multiplies of 3 or 5
- *
- * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
- * The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+ * Link: https://projecteuler.net/problem=1
  */
 
 /*
@@ -10,9 +8,9 @@
  *
  * SET sum to 0
  *
- * FOR each number starting from 3 until 999
- *   IF number is multiple of 3 or 5
- *      SET sum to sum + number
+ * FOR each number (i) from 3 to 999
+ *   IF i is multiple of 3 or 5
+ *      SET sum to sum + i
  *   ENDIF
  * ENDFOR
  *
