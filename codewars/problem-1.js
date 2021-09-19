3578bb04f3ab149e646b6a71cdee76e4568ade96
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2021-09-19 09:56:59 +0700

    feat: add codewars problem-1

---
+++ codewars/problem-1.js
@@ -0,0 +1,37 @@
+/*
+ * Title: Array.diff
+ * Link: https://www.codewars.com/kata/523f5d21c841566fde000009
+ */
+
+/*
+ * Pseudocode
+ *
+ * ARGUMENT a 
+ * ARGUMENT b
+ *
+ * SET filteredArray to []
+ *
+ * FOR each element in a
+ *    IF element doesn't exist in b
+ *        PUSH it into filteredArray
+ *
+ * RETURN filteredArray
+ */
+
+function arrayDiff(a, b) {
+  const filteredArray = [];
+
+  for (elem of a) {
+    if (!b.includes(elem)) {
+      filteredArray.push(elem);
+    }
+  }
+
+  return filteredArray;
+}
+
+function arrayDiff2(a, b) {
+  return a.filter(elem => !b.includes(elem));
+}
+
+console.log(arrayDiff2([1, 2, 3, 2], [2]));
